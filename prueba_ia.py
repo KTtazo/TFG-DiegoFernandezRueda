1b3f0d00bd71b8076dcf47782361a9e7a95a1c82
DiegoEwin <diego.fernandezr@edu.uah.com>
2022-12-24 15:05:40 +0100

    primera creacion de prueba_ia

---
+++ prueba_ia.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+from network import Network
+from fc_layer import FCLayer
+from act_layer import ActivationLayer
+from activation_fun import tanh, tanh_prime
+from losses import mse, mse_prime
